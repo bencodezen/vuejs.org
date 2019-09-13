c426169eb127208cb378b3b5a763b0b4fe134130
Ben Hong <bencodezen@gmail.com>
2019-09-13 11:38:49 -0400

    migrate :truck: Migrate Learn dropdown nav items

--- prototype/.vuepress/config.js
+++ prototype/.vuepress/config.js
@@ -9,7 +9,24 @@ module.exports = {
             text: 'Documentation',
             items: [
               { text: 'Guide', link: '/v2/guide/' },
-              { text: 'API', link: '/v2/api/' }
+              { text: 'API', link: '/v2/api/' },
+              { text: 'Style Guide', link: '/v2/style-guide/' },
+              { text: 'Examples', link: '/v2/examples/' },
+              { text: 'Cookbook', link: '/v2/cookbook/' }
+            ]
+          },
+          {
+            text: 'Video Courses',
+            items: [
+              {
+                text: 'Vue Mastery',
+                link: 'https://www.vuemastery.com/courses/'
+              },
+              {
+                text: 'Vue School',
+                link:
+                  'https://vueschool.io/?friend=vuejs&utm_source=Vuejs.org&utm_medium=Link&utm_content=Navbar%20Dropdown'
+              }
             ]
           }
         ]
